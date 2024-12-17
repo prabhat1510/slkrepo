4593d0a0c1e483128ccae0b55ee1c0210d592a31
Prabhat Chandra <prabhatc@gmail.com>
2024-12-17 14:00:26 +0530

    Added DAO Impl logic  and ppt
    Added DAO Impl logic  and ppt

--- jdbcapp/src/main/java/jdbcapp/dao/DepartmentDAOImpl.java
+++ jdbcapp/src/main/java/jdbcapp/dao/DepartmentDAOImpl.java
@@ -1,17 +1,60 @@
 package jdbcapp.dao;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 import jdbcapp.model.Department;
+import jdbcapp.utility.DBConnectionUtil;
 
 public class DepartmentDAOImpl implements DepartmentDAO {
 
 	public Department addDepartment(Department department) {
-		// TODO Auto-generated method stub
+		String sqlInsert ="insert into department values(?,?);";
+		Connection conn = DBConnectionUtil.getDBConnection();
+		PreparedStatement pStmt;
+		try {
+			pStmt = conn.prepareStatement(sqlInsert);
+			pStmt.setInt(1, department.getDeptNo());
+			pStmt.setString(2, department.getDeptName());
+			int row = pStmt.executeUpdate();
+			if(row !=0) {
+				return department;
+			}
+			
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		return null;
 	}
 
 	public Department getDepartment(Integer deptNo) {
-		// TODO Auto-generated method stub
-		return null;
+		
+		Department department = null;
+		Connection conn = DBConnectionUtil.getDBConnection();
+		
+		try {
+			PreparedStatement pStmt = conn.prepareStatement("select * from department where deptno=?");
+			pStmt.setInt(1, deptNo);
+			ResultSet rs = pStmt.executeQuery();
+			while(rs.next()) {
+				department = new Department();
+				department.setDeptNo(rs.getInt("deptno"));
+				department.setDeptName(rs.getString(2));
+				
+			}
+			
+			pStmt.close();
+			rs.close();
+			conn.close();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		return department; 
 	}
 
 	public Department updateDepartment(Department department) {
@@ -20,8 +63,22 @@ public class DepartmentDAOImpl implements DepartmentDAO {
 	}
 
 	public String deleteDepartment(Integer deptNo) {
-		// TODO Auto-generated method stub
-		return null;
+		String sqlDelete ="delete from department where deptno=?;";
+		Connection conn = DBConnectionUtil.getDBConnection();
+		PreparedStatement pStmt;
+		try {
+			pStmt=conn.prepareStatement(sqlDelete);
+			pStmt.setInt(1, deptNo);
+			int row = pStmt.executeUpdate(sqlDelete);
+			if(row == 1) {
+				return "Department will dept number "+deptNo+" deleted successfully";
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return "Unable to department with dept number "+deptNo;
+		
 	}
 
 }
