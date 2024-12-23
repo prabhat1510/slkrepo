c8a1bb127a7b4173b0ca2c004b37d6dcac100501
Prabhat Chandra <prabhatc@gmail.com>
2024-12-23 12:05:38 +0530

    Added the form
    Added the form

--- helloworld/src/main/java/com/training/demo/controller/HelloWorldController.java
+++ helloworld/src/main/java/com/training/demo/controller/HelloWorldController.java
@@ -14,42 +14,46 @@ import com.training.demo.service.HelloWorldService;
 @Controller
 public class HelloWorldController {
 
-	@Autowired 
-	private HelloWorldService service;//Ddependency of service class is injected
+	@Autowired
+	private HelloWorldService service;// Ddependency of service class is injected
 
-	//Below is handler method to handle HTTP GET method with uri /hello
-	//http://localhost:8080/hello
-	//@RequestMapping(value = "/hello", method = RequestMethod.GET)
+	// Below is handler method to handle HTTP GET method with uri /hello
+	// http://localhost:8080/hello
+	// @RequestMapping(value = "/hello", method = RequestMethod.GET)
 	@GetMapping("/hello")
 	public ModelAndView getHelloMessage() {
 		String msg = service.getHelloMessage();// Making a call to the service layer to retrieve the data or model
 		ModelAndView mv = new ModelAndView();
-		mv.addObject("message", msg);//msg is the model which we want to send as part of the response
+		mv.addObject("message", msg);// msg is the model which we want to send as part of the response
 		mv.setViewName("hello");// This view name hello is the name of the hello.html
 		return mv;
-	}	 
-	
-	
-	//Below method will be called on submit of html form
-	@RequestMapping(value="/addmessage",method=RequestMethod.POST)
+	}
+
+	// Below method will be called on submit of html form
+	@RequestMapping(value = "/addmessage", method = RequestMethod.POST)
 	public ModelAndView addMessage(@ModelAttribute Message message) {
-		//Making a call to service layer
+		// Making a call to service layer
 		String newMessage = service.addMessage(message);
-		
-		//After process the logic in the service we get response
-		//So depending on the response we are creating a ModalAndView response 
+
+		// After process the logic in the service we get response
+		// So depending on the response we are creating a ModalAndView response
 		ModelAndView mv = new ModelAndView();
-		
-		if(newMessage != null) {
+
+		if (newMessage != null) {
 			mv.addObject("successmsg", "Message added successfully");
 			mv.setViewName("success");
-		}else {
+		} else {
 			mv.addObject("errormsg", "Unable to add message");
 			mv.setViewName("error");
 		}
-		
+
 		return mv;
 	}
-	
-	
+
+	@GetMapping("/getmessageform")
+	public ModelAndView getMessageForm(@ModelAttribute Message message) {
+		ModelAndView mv = new ModelAndView();
+		mv.setViewName("messageform");
+		return mv;
+	}
 }
