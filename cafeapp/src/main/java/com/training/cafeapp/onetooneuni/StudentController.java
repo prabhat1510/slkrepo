611d717fb428ae1b06b4b7ac0227d92a271ec15b
Prabhat Chandra <prabhatc@gmail.com>
2024-12-27 11:01:45 +0530

    Added OneToOne Uni Mapping Delete Retrieve Operation example
    Added OneToOne Uni Mapping Delete Retrieve Operation example

--- cafeapp/src/main/java/com/training/cafeapp/onetooneuni/StudentController.java
+++ cafeapp/src/main/java/com/training/cafeapp/onetooneuni/StudentController.java
@@ -1,9 +1,13 @@
 package com.training.cafeapp.onetooneuni;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -16,5 +20,16 @@ public class StudentController {
 	@PostMapping("/newstudent")
 	public Student newStudent(@RequestBody Student student) {
 		return service.createStudent(student);
+	}
+	
+	@GetMapping("/student")
+	public Student getStudent(@RequestParam("id") Integer studentId) {
+		return service.retrieveStudent(studentId);
+	}
+	
+	@DeleteMapping("/remove/{id}")
+	public String deleteStudent(@PathVariable("id")Integer studentId) {
+		service.deleteStudentById(studentId);
+		return "Student deleted successfully";
 	}
 }
