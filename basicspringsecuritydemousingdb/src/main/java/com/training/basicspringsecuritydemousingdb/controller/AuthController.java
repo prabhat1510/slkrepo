bd67bfc1bc7f5c22177b106c12ca82e5598f4478
Prabhat Chandra <prabhatc@gmail.com>
2024-12-31 10:30:45 +0530

    Added User Registration Code in Controller
    Added User Registration Code in Controller

--- basicspringsecuritydemousingdb/src/main/java/com/training/basicspringsecuritydemousingdb/controller/AuthController.java
+++ basicspringsecuritydemousingdb/src/main/java/com/training/basicspringsecuritydemousingdb/controller/AuthController.java
@@ -1,5 +1,8 @@
 package com.training.basicspringsecuritydemousingdb.controller;
 
+import java.util.HashSet;
+import java.util.Set;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -7,7 +10,10 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.training.basicspringsecuritydemousingdb.entities.ERole;
+import com.training.basicspringsecuritydemousingdb.entities.Role;
 import com.training.basicspringsecuritydemousingdb.entities.UserEntity;
+import com.training.basicspringsecuritydemousingdb.repositories.RoleRepository;
 import com.training.basicspringsecuritydemousingdb.repositories.UserRepository;
 import com.training.basicspringsecuritydemousingdb.security.payload.request.SignupRequest;
 import com.training.basicspringsecuritydemousingdb.security.payload.response.MessageResponse;
@@ -19,6 +25,9 @@ public class AuthController {
 
 	@Autowired
 	private UserRepository userRepository;
+	
+	@Autowired
+	private RoleRepository roleRepository;
 	
 	@Autowired
 	private PasswordEncoder encoder;
@@ -42,9 +51,8 @@ public class AuthController {
 		UserEntity user = new UserEntity(signUpRequest.getUsername(), signUpRequest.getEmail(),
 				encoder.encode(signUpRequest.getPassword()));
 		
-		//TODO Add roles and other properties
-		
-		user.setRoles(null);
+		//Add roles and other properties
+		user.setRoles(getRoles(signUpRequest));
 		user.setAddress(signUpRequest.getAddress());
 		user.setEmail(signUpRequest.getEmail());
 		user.setMobile(signUpRequest.getMobile());
@@ -54,5 +62,45 @@ public class AuthController {
 
 		return ResponseEntity.ok(new MessageResponse("User registered successfully!"));
 
+	}
+	
+	//Get Roles from DB if not present in SignupRequest
+	public Set<Role> getRoles(SignupRequest signupRequest){
+		Set<String> strRoles = signupRequest.getRole();
+		Set<Role> roles = new HashSet<>();
+
+		if (strRoles == null) {
+			Role userRole = roleRepository.findByName(ERole.ROLE_USER)
+					.orElseThrow(() -> new RuntimeException("Error: Role is not found."));
+			roles.add(userRole);
+		} else {
+			strRoles.forEach(role -> {
+				switch (role) {
+				case "admin":
+					Role adminRole = roleRepository.findByName(ERole.ROLE_ADMIN)
+							.orElseThrow(() -> new RuntimeException("Error: Role is not found."));
+					roles.add(adminRole);
+
+					break;
+				case "child":
+					Role childRole = roleRepository.findByName(ERole.ROLE_CHILD)
+							.orElseThrow(() -> new RuntimeException("Error: Role is not found."));
+					roles.add(childRole);
+
+					break;
+				case "parent":
+					Role parentRole = roleRepository.findByName(ERole.ROLE_PARENT)
+							.orElseThrow(() -> new RuntimeException("Error: Role is not found."));
+					roles.add(parentRole);
+
+					break;
+				default:
+					Role userRole = roleRepository.findByName(ERole.ROLE_USER)
+							.orElseThrow(() -> new RuntimeException("Error: Role is not found."));
+					roles.add(userRole);
+				}
+			});
+		}
+		return roles;
 	}
 }
