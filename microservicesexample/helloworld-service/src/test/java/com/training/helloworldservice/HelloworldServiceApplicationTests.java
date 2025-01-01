dfd5b079124e7b0beb3bc30eadc5e693d4c52c00
Prabhat Chandra <prabhatc@gmail.com>
2025-01-01 11:07:12 +0530

    Added Microservices Example
    Added Microservices Example

---
+++ microservicesexample/helloworld-service/src/test/java/com/training/helloworldservice/HelloworldServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.training.helloworldservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloworldServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
