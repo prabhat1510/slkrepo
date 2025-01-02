316ab973c05442eaa418839cefea42cac7ca15e4
Prabhat Chandra <prabhatc@gmail.com>
2025-01-02 10:37:24 +0530

    Fixed swagger dependency related isssues
    Fixed swagger dependency related isssues

--- microservicesexample/bookrestapi/src/main/java/com/training/bookrestapi/BookrestapiApplication.java
+++ microservicesexample/bookrestapi/src/main/java/com/training/bookrestapi/BookrestapiApplication.java
@@ -3,22 +3,24 @@ package com.training.bookrestapi;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
 
 @SpringBootApplication
-@EnableDiscoveryClient
 public class BookrestapiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BookrestapiApplication.class, args);
 	}
-	/**
+
 	@Value("${app.title}")
 	private String title;
 	
 	@Value("${app.license}")
 	private String license;
-	
 	@Bean
 	public OpenAPI customOpenAPI(@Value("${application-description}") String appDesciption,
 			@Value("${application-version}") String appVersion) {
@@ -27,5 +29,5 @@ public class BookrestapiApplication {
 		return new OpenAPI().info(new Info().title(title).version(appVersion)
 				.description(appDesciption).termsOfService("http://swagger.io/terms/")
 				.license(new License().name(license).url("http://springdoc.org")));
-	}**/
+	}
 }
