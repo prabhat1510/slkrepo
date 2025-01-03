3872e6e48d17a4d95eedc07c4440e3b9219b58b9
Prabhat Chandra <prabhatc@gmail.com>
2025-01-03 16:59:13 +0530

    Service Layer Unit Test case
    Service Layer Unit Test case

--- restapiunittestdemo/src/main/java/com/training/restapiunittestdemo/service/StudentServiceImpl.java
+++ restapiunittestdemo/src/main/java/com/training/restapiunittestdemo/service/StudentServiceImpl.java
@@ -1,13 +1,18 @@
 package com.training.restapiunittestdemo.service;
 
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.training.restapiunittestdemo.entities.Student;
 import com.training.restapiunittestdemo.exceptions.StudentNotFoundException;
+import com.training.restapiunittestdemo.repositories.StudentRepository;
 
 @Service
 public class StudentServiceImpl implements StudentService {
-
+	@Autowired
+	private StudentRepository repo;
 	@Override
 	public Student addNewStudent(Student student) {
 		// TODO Auto-generated method stub
@@ -16,8 +21,12 @@ public class StudentServiceImpl implements StudentService {
 
 	@Override
 	public Student getStudentById(Integer studentId) throws StudentNotFoundException {
-		// TODO Auto-generated method stub
-		return null;
+		Optional<Student> student = repo.findById(studentId);
+		if(student.isPresent()) {
+			return student.get();
+		}else {
+			throw new StudentNotFoundException("Student with student id "+studentId+" not found");
+		}
 	}
 
 	@Override
--- restapiunittestdemo/src/test/java/com/training/restapiunittestdemo/service/StudentServiceImplTests.java
+++ restapiunittestdemo/src/test/java/com/training/restapiunittestdemo/service/StudentServiceImplTests.java
@@ -1,5 +1,39 @@
 package com.training.restapiunittestdemo.service;
 
-public class StudentServiceImplTests {
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import com.training.restapiunittestdemo.entities.Student;
+import com.training.restapiunittestdemo.exceptions.StudentNotFoundException;
+
+@SpringBootTest
+public class StudentServiceImplTests {
+	
+	@Autowired
+	private StudentService service;
+
+	@Test
+	void testGetStudent() {
+		Student student = null;
+		try {
+			student = service.getStudentById(2);
+		} catch (StudentNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		assertNotNull(student);
+		assertTrue(student.getStudentName().equals("Sujan"));
+		
+	}
+	
+	@Test
+	void testGetStudentThrowingException() {
+		assertThatThrownBy(()->service.getStudentById(21)).isInstanceOf(StudentNotFoundException.class);
+	}
 }
